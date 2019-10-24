2644c23533d93dd3e9d800c855ba009d160c1741
Diella Agyeman <diellaagyeman@hotmail.co.uk>
2019-10-24 22:46:57 +0100

    header, dropdown and card container components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContainerComponent } from './components/container/container.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContainerComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/container/container.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.scss']
+})
+export class ContainerComponent implements OnInit {
+
+  searchBrand:string = "maybelline";
+
+  cardImage = "../../../assets/mirror.png";
+  altText = "card image";
+
+  url = `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${this.searchBrand}`;
+
+  // fetch(url)
+  // .then((response) => response.json()) 
+  // .then((data) => {  
+      
+  // })
+  // .catch((error) => {
+  //     throw new Error(error);
+  // });
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
