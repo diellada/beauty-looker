3d8b7daf946a70c8e2b86c93de065315c2710a20
Diella Agyeman <diellaagyeman@hotmail.co.uk>
2019-11-01 13:16:35 +0000

    card code clean up

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -10,22 +10,6 @@ export class CardComponent implements OnInit {
 
   @Input() product: IBrandData;
 
-  // i = 0;
-  // testData:IBrandData = {
-  //   brand: fentyProducts[this.i].brand,
-  //   name: fentyProducts[this.i].name,
-  //   price: fentyProducts[this.i].price,
-  //   poster: fentyProducts[this.i].image_link,
-  //   productLink: fentyProducts[this.i].product_link,
-  //   category: fentyProducts[this.i].category,
-  //   productType: fentyProducts[this.i].product_type,
-  //   description: fentyProducts[this.i].description
-  // };
-  
-  // cardImage = this.testData.poster;
-  // name = this.testData.name;
-  // brand = this.testData.brand;
-
   constructor() { }
 
   ngOnInit() {
