0c9b6b7bfe15258a446c518d7c5e8f82010ecfbd
Diella Agyeman <diellaagyeman@hotmail.co.uk>
2019-11-05 20:19:53 +0000

    added price info to cards, fixed some navbar scss bugs, added favourite icon and mouse over logic

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IBrandData } from "src/assets/data/mockdata";
+import { faHeart as faHeartSolid} from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faHeartOpen } from "@fortawesome/free-regular-svg-icons"
 
 @Component({
   selector: 'app-card',
@@ -9,10 +11,13 @@ import { IBrandData } from "src/assets/data/mockdata";
 export class CardComponent implements OnInit {
 
   @Input() product: IBrandData;
+  faHeart = faHeartOpen;
 
   constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  changeIcon() {
+    this.faHeart === faHeartOpen ? this.faHeart = faHeartSolid : this.faHeart = faHeartOpen;
   }
 
 }
